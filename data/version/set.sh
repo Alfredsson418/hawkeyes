f251e815d70818fa9551a108287c5c315ae9cf9a
Oskar Alfredsson <oskar.alfredsson@proton.me>
2025-04-08 18:54:38 +0200

    Fixed hookes and workflows

--- data/version/set.sh
+++ data/version/set.sh
@@ -2,10 +2,23 @@
 
 path="./data/version/version.json"
 
-new_release=$(($1))
-new_version=$(($2))
-new_patch=$(($3))
+old=$(jq -r ".version" "$path")
 
+new_release=$(echo "$old" | cut -d '.' -f 1)
+new_version=$(echo "$old" | cut -d '.' -f 2)
+new_patch=$(echo "$old" | cut -d '.' -f 3)
+
+if [[ $1 != "-1" ]]; then
+    new_release=$(($1))
+fi
+
+if [[ $2 != "-1" ]]; then
+    new_version=$(($2))
+fi
+
+if [[ $3 != "-1" ]]; then
+    new_patch=$(($3))
+fi
 
 
 new="{\"version\": \"${new_release}.${new_version}.${new_patch}\"}"
